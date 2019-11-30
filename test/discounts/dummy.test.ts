5631ed9c35b446d6bd7427fdf06ef5be4bfbc596
mkommar <mahesh.kommareddi@gmail.com>
2019-11-30 01:09:18 -0500

    Dummy implementation done. Dummy test done. Naive implementation done. Naive Test nearly done.

--- test/discounts/dummy.test.ts
+++ test/discounts/dummy.test.ts
@@ -4,20 +4,16 @@ import { Cart } from "../../src/model/cart";
 import  items from '../../src/data/skus.json';
 
 test('check dummy discount', () => {
-    let baseSkus = new Array<SKU>();
+    let baseCart = new Array<SKU>();
     let discountCart = new Array<SKU>();
     let discountModel = new DummyDiscount();
 
-    items.forEach(item => baseSkus.push(new SKU(item.sku, item.name, item.price, item.qty)));
+    items.forEach(item => baseCart.push(new SKU(item.sku, item.name, item.price, item.qty)));
     
-    let cart = new Cart(baseSkus, discountCart, discountModel);
+    let cart = new Cart(baseCart, discountCart, discountModel);
     cart.runDiscountModel();
 
-    test('check discount on dummy', () => {
-        for(let i=0; i < cart.baseCart.length; i++) {
-            expect(cart.baseCart[i].sku).toBe(cart.discountCart[i].sku);
-        }
-    })
-
-    
+    for(let i=0; i < cart.baseCart.length; i++) {
+        expect(cart.baseCart[i].sku).toBe(cart.discountCart[i].sku);
+    }
 })
--- test/discounts/naive.test.ts
+++ test/discounts/naive.test.ts
@@ -4,13 +4,21 @@ import { Cart } from "../../src/model/cart";
 import  items from '../../src/data/skus.json';
 
 test('check naive discount', () => {
-    let baseSkus = new Array<SKU>();
+    let baseCart = new Array<SKU>();
     let discountCart = new Array<SKU>();
     let discountModel = new NaiveDiscount();
 
-    items.forEach(item => baseSkus.push(new SKU(item.sku, item.name, item.price, item.qty)));
+    items.forEach(item => baseCart.push(new SKU(item.sku, item.name, item.price, item.qty)));
     
-    let cart = new Cart(baseSkus, discountCart, discountModel);
+    let cart = new Cart(baseCart, discountCart, discountModel);
     cart.runDiscountModel();
+
+    cart.discountCart.forEach(item => {
+        // Count macs and rpis. Should be same
+
+        // Count Google Homes. The total price should be appropriate
+
+        // Total Alexas, should be 90% of total
+    })
 
 })
