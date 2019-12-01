419aaf9b801367485895605b357dc67c07e27237
mkommar <mahesh.kommareddi@gmail.com>
2019-12-01 16:22:03 -0500

    First working endpoint based on the naive model

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,43 @@
 import express from "express";
+import { NaiveDiscount } from "./module/naive/model/discount";
+import { SKU } from "./model/sku";
+import { Cart } from "./model/cart";
+import  items from './data/skus.json';
+
+let baseCart = new Array<SKU>();
+let discountCart = new Array<SKU>();
+let discountModel = new NaiveDiscount();
+
+items.forEach((item: { sku: string; name: string; price: number; qty: number; }) => baseCart.push(new SKU(item.sku, item.name, item.price, item.qty)));
+
+let cart = new Cart(baseCart, discountCart, discountModel);
+cart.runDiscountModel();
 
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
+function strMapToObj(strMap: Map<string, SKU> ) {
+    let obj = Object.create(null);
+    for (let [k,v] of strMap) {
+        // We don’t escape the key '__proto__'
+        // which can cause problems on older engines
+        obj[k] = v;
+    }
+    return obj;
+}
 
-app.listen(port, () => console.log(`Server started listening to port ${port}`));
+app.get("/", (req, res) => {
+    let globalBaseCart = strMapToObj(cart.baseCartMap);
+    let globalDiscountCart = strMapToObj(cart.discountCartMap);
+    let out = {
+        cart: globalBaseCart,
+        discount: globalDiscountCart
+    }
+    res.json(out);
+});
+
+app.listen(port, () => 
+{
+    // tslint:disable-next-line:no-console
+    console.log(`Server started listening to port ${port}`)
+});
