19dfb98ac540500b25483eaa8e7fc28f77e35877
mkommar <mahesh.kommareddi@gmail.com>
2019-12-01 11:49:04 -0500

    Broken the main branch. Found a huge flaw. We need an inventory tracking system.

--- test/discounts/naive.test.ts
+++ test/discounts/naive.test.ts
@@ -13,12 +13,22 @@ test('check naive discount', () => {
     let cart = new Cart(baseCart, discountCart, discountModel);
     cart.runDiscountModel();
 
-    cart.discountCart.forEach(item => {
-        // Count macs and rpis. Should be same
+    let baseMacBookProCriteria = cart.baseCart.filter(sku => sku.sku === "43N23P");
+    let baseRPiCriteria = cart.baseCart.filter(sku => sku.sku === "234234");
+    let baseGoogleHomeCriteria = cart.baseCart.filter(sku => sku.sku === "120P90");
+    let baseAlexaCriteria = cart.baseCart.filter(sku => sku.sku === "A304SD");
 
-        // Count Google Homes. The total price should be appropriate
+    let discountMacBookProCriteria = cart.discountCart.filter(sku => sku.sku === "43N23P");
+    let discountRPiCriteria = cart.baseCart.filter(sku => sku.sku === "234234");
+    let discountGoogleHomeCriteria = cart.discountCart.filter(sku => sku.sku === "120P90");
+    let discountAlexaCriteria = cart.discountCart.filter(sku => sku.sku === "A304SD");
 
-        // Total Alexas, should be 90% of total
-    })
+    // Count macs and rpis. Should be same
+    expect(discountMacBookProCriteria.length).toBeLessThanOrEqual(discountRPiCriteria.length);
 
+    // Count Google Homes. The total price should be appropriate
+    expect((baseGoogleHomeCriteria.length * baseGoogleHomeCriteria[0].price)).toBeLessThanOrEqual(discountGoogleHomeCriteria.length * discountGoogleHomeCriteria[0].price)
+    
+    // Total Alexas, should be 90% of total
+    expect(baseAlexaCriteria.length * baseAlexaCriteria[0].price *.9).toBeLessThanOrEqual(discountAlexaCriteria.length * discountAlexaCriteria[0].price)
 })
